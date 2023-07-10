80ebaa5136ddb4655da7c83d57cbc7ffd945be38
alexop345 <alexandru.oprea@rodeapps.com>
2023-07-10 10:37:39 +0300

    flavor_config file

--- lib/main_prod.dart
+++ lib/main_prod.dart
@@ -1,7 +1,12 @@
 import 'package:albums/app.dart';
+import 'package:albums/flavor_config.dart';
 import 'package:flutter/material.dart';
 
 void main() {
+  FlavorConfig(
+    flavor: Flavor.dev,
+    values: FlavorValues(baseUrl: 'https://jsonplaceholder.typicode.com/'),
+  );
   runApp(
     const App(),
   );
